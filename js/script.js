c534dacad23d47e76d7fb883de897c0c147abf0d
Kate <Kate>
2016-10-09 12:27:45 +0300

    next cool commit

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@
 $( document ).ready(function() {
 
 function changeContent() {
-    if ($("#two").is(':visible')){
+    if ($("#two").is(':hidden')){
         showList();
         alert('to page 2');
     }
